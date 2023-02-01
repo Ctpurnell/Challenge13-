3bfc8f80ee1f7b5c17936b381e319c80c145c5ff
Christopher Purnell <112015234+Ctpurnell@users.noreply.github.com>
2023-02-01 14:16:35 -0500

    set up models

--- models/Product.js
+++ models/Product.js
@@ -10,7 +10,41 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      isDecimal: true
+    },
+
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      isNumeric: true
+    },
+
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+        unique: false
+      }
+    }
   },
+  
   {
     sequelize,
     timestamps: false,
@@ -21,3 +55,5 @@ Product.init(
 );
 
 module.exports = Product;
+
+
