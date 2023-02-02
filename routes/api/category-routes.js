07792d1e5357670bf92f97c2829dbbbd881eaa9f
Christopher Purnell <112015234+Ctpurnell@users.noreply.github.com>
2023-02-02 16:23:16 -0500

    fixing code

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -9,7 +9,7 @@ router.get("/", async (req, res) => {
     const categoryData = await Category.findAll({
       include: [{ model: Product }],
     });
-    res.status(200).json(productData);
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -20,7 +20,7 @@ router.get("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
   try {
     const categoryData = await Category.findByPk(req.params.id, {
-      include: [{ model: Product }],
+      include: [{ model: Category }, { model: Product }],
     });
     if (!categoryData) {
       res.status(404).json({ message: "Not Found!" });
